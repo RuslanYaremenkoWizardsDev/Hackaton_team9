babdd05305ebfdf5cefd568030abd161779a7bfa
Vita Redko <rvitargo@gmail.com>
2021-05-15 21:10:00 +0300

    add UserRepository

--- backend/src/main/java/com/github/repository/UsersRepository.java
+++ backend/src/main/java/com/github/repository/UsersRepository.java
@@ -4,12 +4,13 @@ import com.github.dto.UserAuthorizationDto;
 import com.github.dto.UserRegistrationDto;
 import com.github.entity.User;
 import com.github.utils.HibernateUtils;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class UsersRepository {
 
-    public void saveStudent(User user) {
+    public User save(User user) {
         Transaction transaction = null;
         try (Session session = HibernateUtils.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
@@ -21,68 +22,76 @@ public class UsersRepository {
             }
             e.printStackTrace();
         }
+        return user;
     }
+
+    public User findByEmail(User user){
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            user =  session.load(User.class,
+                    user.getEmail());
+            Hibernate.initialize(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return user;
+    }
+
+    public User findByNickname(User user){
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            user =  session.load(User.class,
+                    user.getNickname());
+            Hibernate.initialize(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return user;
+    }
+
+    public void update(User user){
+        Transaction transaction = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(User user){
+        Transaction transaction = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+
 
     public static void main(String[] args) {
         UsersRepository usersRepository = new UsersRepository();
-        usersRepository.saveStudent(new User());
-    }
-
-    public User findByAuthDto(UserAuthorizationDto userAuthorizationDto){
-//        return jdbcTemplate.findBy(
-//                "SELECT * FROM users WHERE login = ? AND password = ?",
-//                RowMappers.getCustomRowMapperUser(),
-//                userAuthorizationDto.getLogin(),
-//                userAuthorizationDto.getPassword()
-//        );
-        return null;
-    }
-
-    public User findById(long id){
-//        return jdbcTemplate.findBy(
-//                "SELECT * FROM users WHERE id = ?",
-//                RowMappers.getCustomRowMapperUser(),
-//                id
-//        );
-        return null;
-    }
-
-    public User insert(UserRegistrationDto userRegistrationDto){
-        System.out.println("INSERT");
-//        return jdbcTemplate.insert(
-//                "INSERT INTO users (first_name, last_name, email, login, password, phone_number) VALUES(?, ?, ?, ?, ?, ?)",
-//                RowMappers.getCustomRowMapperUser(),
-//                userRegistrationDto.getFirstName(),
-//                userRegistrationDto.getLastName(),
-//                userRegistrationDto.getEmail(),
-//                userRegistrationDto.getLogin(),
-//                userRegistrationDto.getPassword(),
-//                userRegistrationDto.getPhone()
-//        );
-        return null;
-    }
-
-    public void delete(UserRegistrationDto userRegistrationDto){
-//        jdbcTemplate.delete(
-//                "DELETE FROM users WHERE login = ?",
-//                userRegistrationDto.getLogin()
-//        );
-        return;
-    }
-
-    public void update(UserRegistrationDto userRegistrationDto){
-//        jdbcTemplate.update(
-//                "UPDATE users " +
-//                        "SET first_name = ?, last_name = ?, email = ?, login = ?, password = ?, phone_number = ?" +
-//                        "WHERE login = ?",
-//                userRegistrationDto.getFirstName(),
-//                userRegistrationDto.getLastName(),
-//                userRegistrationDto.getEmail(),
-//                userRegistrationDto.getLogin(),
-//                userRegistrationDto.getPassword(),
-//                userRegistrationDto.getPhone(),
-//                userRegistrationDto.getLogin()
-//                );
+        usersRepository.save(new User());
     }
 
 }
