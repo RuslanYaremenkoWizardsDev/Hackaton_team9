6c702472989929832d740d769578242ab53ddd32
Alex_M <iipo1004ek@gmail.com>
2021-05-15 19:50:49 +0300

    add reg page

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,9 @@
-import './styles/index.scss'
+import { validation } from './modules';
+import './styles/index.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-})
+  const path = location.pathname;
+  if (path === '/' || path === '/index.html') {
+    validation();
+  }
+});
---
+++ front/src/modules/helpers/urls.js
@@ -0,0 +1,5 @@
+const urls = {
+  auth: '/auth',
+  registr: ''
+};
+export default urls;
