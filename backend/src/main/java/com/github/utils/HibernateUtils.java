92b8813c45d9dc3a39009013ca90949ea8fee640
Vita Redko <rvitargo@gmail.com>
2021-05-15 18:33:39 +0300

    add Hibernate utils

--- backend/src/main/java/com/github/utils/HibernateUtils.java
+++ backend/src/main/java/com/github/utils/HibernateUtils.java
@@ -1,29 +1,47 @@
 package com.github.utils;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
+import java.util.Properties;
+
+import com.github.entity.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtils {
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
-    private static final Logger log = LoggerFactory.getLogger(HibernateUtils.class);
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, "jdbc:postgresql://ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/d1hoh4lbrra3ie");
+                settings.put(Environment.USER, "wurpiknjdxmvik");
+                settings.put(Environment.PASS, "447fb722d618c71efed9ceb0266cf9228b2ae40869168b598b8c9925bbe99a80");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
 
-    private static final SessionFactory sessionFactory;
-    static {
-        try {
-            sessionFactory = new AnnotationConfiguration()
-                    .configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            log.error("Error session configuration!");
-            throw new ExceptionInInitializerError(ex);
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    public static Session getSession()
-            throws HibernateException {
-        return sessionFactory.openSession();
+        return sessionFactory;
     }
 }
--- backend/src/main/java/com/github/utils/PropsLoader.java
+++ backend/src/main/java/com/github/utils/PropsLoader.java
@@ -1,8 +1,5 @@
 package com.github.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
