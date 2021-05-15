04c1d712b3caa02946e08ac7d3372929186d069b
Alex_M <iipo1004ek@gmail.com>
2021-05-16 01:12:42 +0300

    end registration authorization

--- front/src/modules/mainPage/renderModal.js
+++ front/src/modules/mainPage/renderModal.js
@@ -1,31 +1,31 @@
-import {modal} from '..';
+import { modal } from '..';
 
-export default function renderModal(){
-    let settingsBtn=document.querySelector(".header__set-img");
-    let settingsBox=document.querySelector('.header__settings');
+export default function renderModal() {
+    let settingsBtn = document.querySelector('.header__set-img');
+    let settingsBox = document.querySelector('.header__settings');
 
-    settingsBtn.addEventListener("click",(e)=>{
-        let modalBox=document.createElement('div')
+    settingsBtn.addEventListener('click', (e) => {
+        let modalBox = document.createElement('div');
 
-        modalBox.innerHTML=modal
-        settingsBox.appendChild(modalBox)
+        modalBox.innerHTML = modal;
+        settingsBox.appendChild(modalBox);
         let modalSettings = document.querySelector('.header__modal');
 
-        modalSettings.style.display = 'block'
+        modalSettings.style.display = 'block';
         let modalClose = document.querySelector('.header__close');
 
-        modalClose.addEventListener('click',(e)=>{    
-            if (e.target.className == "header__close"){
-            modalSettings.style.display = 'none';
+        modalClose.addEventListener('click', (e) => {
+            if (e.target.className == 'header__close') {
+                modalSettings.style.display = 'none';
             }
-        })
+        });
     });
 
-    modalClose.addEventListener('click',(e)=>{
-        if (e.target.className == "header__close"){
-        modalSettings.style.display = 'none';
+    modalClose.addEventListener('click', (e) => {
+        if (e.target.className == 'header__close') {
+            modalSettings.style.display = 'none';
         }
-    })
+    });
 
 }
 
