8c302fb12fc3533d2763a1b7e7eecf4e79c8c055
Denys <63046982+DenysFedorovych@users.noreply.github.com>
2021-05-15 17:32:43 +0300

    all fixed

---
+++ backend/src/main/java/com/github/entity/Tournament.java
@@ -0,0 +1,17 @@
+package com.github.entity;
+
+import java.util.Date;
+
+public class Tournament {
+
+    private String length;
+
+    private String description;
+
+    private String mode;
+
+    private String place;
+
+    private Date start;
+
+}
