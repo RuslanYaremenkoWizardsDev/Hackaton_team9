016cd313542442be747dc9c2aa8e1f36b51fb269
Vita Redko <rvitargo@gmail.com>
2021-05-17 01:05:45 +0300

    fixed minor bugs

--- backend/src/main/test/java/com/github/repository/UsersRepositoryTest.java
+++ backend/src/main/test/java/com/github/repository/UsersRepositoryTest.java
@@ -4,12 +4,12 @@ import com.github.config.DatabaseConfig;
 import com.github.dto.UserAuthorizationDto;
 import com.github.dto.UserRegistrationDto;
 import com.github.entity.User;
-import com.github.service.UsersService;
+import com.github.service.DataService;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UsersRepositoryTest {
-    private UsersService repo = DatabaseConfig.getUsersService();
+    private DataService repo = DatabaseConfig.getUsersService();
 
     private UserRegistrationDto userRegistrationDto = new UserRegistrationDto("login","password");
     private UserAuthorizationDto userAuthorizationDto = new UserAuthorizationDto("login","password");
