a46d7655d7d7f8365ce11a2881585a661e3963fb
Alex_M <iipo1004ek@gmail.com>
2021-05-17 04:58:30 +0300

    WTF2

--- front/src/modules/mainPage/addTours.js
+++ front/src/modules/mainPage/addTours.js
@@ -1,7 +1,6 @@
 export default async function addTours() {
   const res = await fetch('/src/db/tours.json');
   const data = await res.json();
-  console.log(data);
   const tbody = document.querySelector('#toursbody');
 
   data.forEach(el => {
--- front/src/modules/mainPage/chengeColorTheme.js
+++ front/src/modules/mainPage/chengeColorTheme.js
@@ -1,21 +1,20 @@
-export function changeColors(){
-let chengeBtn=document.querySelector('.header__color-theme')
-// console.log(chengeBtn)
+export function changeColors() {
+    let chengeBtn = document.querySelector('.header__color-theme');
+    // console.log(chengeBtn)
 
-    chengeBtn.addEventListener("click", ()=>{
-        if(chengeBtn.classList.contains("moon")){
-            chengeBtn.classList.remove('moon')
-            chengeBtn.classList.add('sun')
-            chengeBtn.innerHTML="&#9728;"
-            chengeBtn.style.color="orange"
-            // chengeBtn.setAttribute('src','../../assets/icomoon/PNG/sun.png')
-        }
-        else if(chengeBtn.classList.contains("sun")){
-            chengeBtn.classList.remove('sun')
-            chengeBtn.classList.add('moon')
-            chengeBtn.innerHTML="&#127769;"
+    chengeBtn.addEventListener('click', () => {
+        if (chengeBtn.classList.contains('moon')) {
+            chengeBtn.classList.remove('moon');
+            chengeBtn.classList.add('sun');
+            chengeBtn.innerHTML = '&#9728;';
+            chengeBtn.style.color = 'orange';
 
-            // chengeBtn.setAttribute('src','../../assets/icomoon/PNG/moon.png')
         }
-    })
+        else if (chengeBtn.classList.contains('sun')) {
+            chengeBtn.classList.remove('sun');
+            chengeBtn.classList.add('moon');
+            chengeBtn.innerHTML = '&#127769;';
+
+        }
+    });
 }
